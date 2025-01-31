ff8f58c7eee6199de3153d4c49f4ba474bb95281
Gregor Mustang <110684118+gregor-shkashekwa@users.noreply.github.com>
2025-01-31 19:03:41 +0700

    Update MainWindow.xaml.cs

--- MasterPol_KalashnikovG/MainWindow.xaml.cs
+++ MasterPol_KalashnikovG/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MasterPol_Danilenko
+namespace MasterPol_KalashnikovG
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
